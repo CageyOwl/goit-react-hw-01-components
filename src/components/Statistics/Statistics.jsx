6bb14820554e3ddd5672d788a1e5f787494013b5
CageyOwl <alex1377c9@gmail.com>
2023-03-07 11:39:27 +0200

    First review, conditional render was added.

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,7 +5,7 @@ import Stat from './Stat';
 export default function Statistics({ title, stats }) {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title ? title : 'Some files statistics'}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css['stat-list']}>
         {stats.map(stat => (
